3c6bc27d4ef611f7b2c2525a23e083af937ff825
jacoons <jandrewcoons@gmail.com>
2017-11-29 16:53:56 -0800

    Signed-off-by: jacoons <jandrewcoons@gmail.com>

--- Homework/Assignment_7(Ch.8)/Assignment 7 - Menu/main.cpp
+++ Homework/Assignment_7(Ch.8)/Assignment 7 - Menu/main.cpp
@@ -1,11 +1,5 @@
-/* 
- * File:   main.cpp
- * Author: Dr. Mark E. Lehr
- * Created on Oct 18th, 2017, 9:05 AM
- * Purpose:  Menu with functions
- */
-
-//System Libraries
+#include <string> 
+#include <iomanip>
 #include <iostream>     //Input/Output Stream Library
 using namespace std;    //Standard Name-space under which System Libraries reside
 
@@ -24,6 +18,76 @@ void prob7();
 void prob8();
 void prob9();
 void menu();
+
+void sortArray(string names[], int size)
+{
+    int startScan, minIndex; 
+    string minValue;
+
+    for (startScan = 0; startScan < (size - 1); startScan++)
+    {
+        minIndex = startScan;
+        minValue = names[startScan];
+        for(int index = startScan + 1; index < size; index++)
+        {
+            if (names[index] < minValue)
+            {
+                minValue = names[index];
+                minIndex = index;
+            }
+        }
+        names[minIndex] = names[startScan];
+        names[startScan] = minValue;
+    }
+}
+
+int binarySearch(string names[], int size, string value)
+{
+    int first = 0,             
+        last = size - 1,       
+        middle,                
+        position = -1;         
+    bool found = false;     
+
+    while (!found && first <= last)
+    {
+        middle = (first + last) / 2;     
+        if (names[middle] == value)      
+        {
+            found = true;
+            position = middle;
+        }
+        else if (names[middle] > value)  
+            last = middle - 1;
+        else
+            first = middle + 1;           
+    }
+    return position;
+}
+
+void Linear_search (int array[], int NUM_ELS)
+    {
+        int charge_num;
+        bool found = false;
+        cout<<"Enter account charge number: "<<endl;
+        cin>>charge_num;
+        for(int i = 0; i<NUM_ELS; i++)
+        {
+            if(array[i] == charge_num)
+            {
+                found = true;
+                break;
+            }
+        }
+        if(found)
+        {
+            cout<<"Number is valid."<<endl;
+        }
+        else
+        {
+            cout<<"Number is not valid."<<endl;
+        }
+    }
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -55,11 +119,63 @@ int main(int argc, char** argv) {
 }
 
 void prob1(){
-    cout<<"You are in Problem 1"<<endl;
+    cout<<"Problem 1 - Binary String Search"<<endl;
+    
+    const int NUM_NAMES = 20;
+    string names[NUM_NAMES] = {"Collins, Bill", "Smith, Bart", "Allen, Jim",
+                               "Griffin, Jim", "Stamey, Marty", "Rose, Geri",
+                               "Taylor, Terri", "Johnson, Jill", "Allison, Jeff",
+                               "Looney, Joe", "Wolfe, Bill", "James, Jean",
+                               "Weaver, Jim", "Pore, Bob", "Rutherford, Greg",
+                               "Javens, Renee", "Harrison, Rose", "Setzer, Cathy",
+                               "Pike, Gordon", "Holland, Beth" };
+
+   
+    string empName; 
+    int results;  
+
+   
+    sortArray(names, NUM_NAMES); 
+
+    
+    cout << "Please enter an employee's name: "<<endl; 
+   
+    std::string str;
+    std::getline(cin, str);
+    getline(cin, empName); 
+  
+
+    
+    results = binarySearch(names, NUM_NAMES, empName); 
+
+   
+    if (results == -1)
+        cout << "That name does not exist in the array.\n";
+    else
+    {
+     
+        cout << "That name is found at element " << results;
+        cout << " in the array.\n";
+    }
+    
+    //system("PAUSE");
+    
 }
 
+     
+
+
 void prob2(){
-    cout<<"You are in Problem 2"<<endl;
+    cout<<"Problem 2 - Charge Account Validation"<<endl;
+    int NUM_ELS = 18;
+    const int ARRAY_SIZE = 18;
+    int array [ARRAY_SIZE] = {5658845, 4520125, 7895122, 8777541, 8451277,
+    1302850, 8080152, 4562555, 5552012, 5050552, 7825877, 1250255, 1005231, 
+    6545231, 3852085, 7576651, 7881200, 4581002};
+    
+    Linear_search(array, NUM_ELS);
+    
+    
 }
 
 void prob3(){
@@ -92,14 +208,13 @@ void prob9(){
 
 void menu(){
     //Input Data/Variables
+    cout<<"Assignment 7 - Chapter 8"<<endl;
     cout<<"Choose from the Menu"<<endl;
-    cout<<"1. Problem 1"<<endl;
-    cout<<"2. Problem 2"<<endl;
-    cout<<"3. Problem 3"<<endl;
-    cout<<"4. Problem 4"<<endl;
-    cout<<"5. Problem 5"<<endl;
-    cout<<"6. Problem 6"<<endl;
-    cout<<"7. Problem 7"<<endl;
-    cout<<"8. Problem 8"<<endl;
-    cout<<"9. Problem 9"<<endl;
+    cout<<"1. Problem 1: \tBinary String Search"<<endl;
+    cout<<"2. Problem 2: \tCharge Account Validation"<<endl;
+    cout<<"3. Problem 3: \tLottery Winners"<<endl;
+    cout<<"4. Problem 4: \tLottery Winners Modification"<<endl;
+    cout<<"5. Problem 5: \tRainfall Statistics Modification"<<endl;
+    cout<<"6. Problem 6: \tSorting Orders"<<endl;
+
 }
